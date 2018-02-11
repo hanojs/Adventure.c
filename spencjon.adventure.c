36901834778f4b296743348a69ee7253b3101f35
jonah1217 <spencjon@oregonstate.edu>
2018-02-11 14:07:50 -0800

    Bug fixes

--- spencjon.adventure.c
+++ spencjon.adventure.c
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <stdio.h>
 
 
 #ifndef AD_VALUES
@@ -20,7 +21,7 @@
 
 struct room {
   char roomName[AD_NAME_INITIAL];
-  char roomType[20]
+  char roomType[20];
   int connections[AD_MAX_CONN];
 };
 struct path {
@@ -33,18 +34,18 @@ struct path {
 void getNewestDirectory(char directoryName[250]){
     DIR *directoryPointer = opendir(".");
     struct stat dirStat;
-    struct dirent *direntPointer
+    struct dirent *direntPointer;
     time_t latest = 0;
     direntPointer = readdir(directoryPointer);
     while (direntPointer != NULL) { //while there are more directories
         memset(&dirStat, 0, sizeof(dirStat)); //allocate the memorY
 
         if ((dirStat.st_mode & S_IFDIR) != S_IFDIR) continue; //IF IT ISN'T A directory
-        if (stat(direntPointer->d_name, &dStat) < 0) continue; //if it can't be gotten into..
+        if (stat(direntPointer->d_name, &dirStat) < 0) continue; //if it can't be gotten into..
 
         if (dirStat.st_mtime > latest) { //if the directory is newer than the old one...
             strcpy(directoryName, direntPointer->d_name);
-            latest = fStat.st_mtime;
+            latest = dirStat.st_mtime;
         }
     }
     closedir(directoryPointer);
