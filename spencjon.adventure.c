7bb51bd6ff55cb54e63e454fb7d35a0e9bed1603
jonah1217 <spencjon@oregonstate.edu>
2018-02-11 18:36:40 -0800

    asd

--- spencjon.adventure.c
+++ spencjon.adventure.c
@@ -57,15 +57,15 @@ void storeRoom(FILE *fs, struct room *rooms, int roomNum){
     int i = 0;
     char *buff = malloc(100 * sizeof(char));
     char *name = malloc(20 * sizeof(char));
-
+    size_t nSize = sizeof(buff);
     //Read/store the room name
-    getline(buff, &(sizeof(buff)), fs);
+    getline(buff, &nsize, fs);
     printf("ROOM NAME %s \n", &buff[12]);
     //memcpy(rooms[roomNum].roomName, &buff[12], sizeof(rooms[roomNum].roomName));
 
 
     //Read/Store all the connections. The last fget will read the room type
-    getline(buff, &(sizeof(buff)), fs);
+    getline(buff, &nSize, fs);
     printf("CONNECTION %s\n", &buff[15]);
     while(buff[0] == 'C'){
       //memcpy(rooms[roomNum].connections[i], &buff[15], sizeof(rooms[roomNum].connections[i]));
